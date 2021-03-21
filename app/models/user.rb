eb9a0fe228a61b227e586b5fc8d530586e619409
kohei <tk1124e25@gmail.com>
2021-03-21 17:48:52 +0900

    commit hello_world

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User
+  def initialize
+    @first_name = "kohei"
+    @last_name = "Tachiki"
+    @birthday = ""
+    @age = 28
+    @birthplace = "aichi/Miyoshi"
+    @hobby = "Guitar"
+  end
